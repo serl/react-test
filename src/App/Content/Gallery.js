5ac4efa07e1edf2062d7cfe272a91d1b919749e1
Sergio Livi <me@serl.it>
2019-05-30 14:41:02 +0200

    styles for gallery

--- src/App/Content/Gallery.js
+++ src/App/Content/Gallery.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import './Gallery.css'
+
+function GalleryGroup({ items, level = 0 }) {
+  return (
+    <div className={`gallery level-${level} level-${level % 2 ? 'odd' : 'even'}`}>
+      {
+        items.map((item, i) => (
+          Array.isArray(item) ?
+            <GalleryGroup
+              key={i}
+              items={item}
+              level={level + 1}
+            />
+            :
+            <img
+              key={i}
+              src={`${process.env.PUBLIC_URL}/${item.url}`}
+              alt={item.alt}
+            />
+        ))
+      }
+    </div>
+  );
+}
 
 function Gallery({ images }) {
   return (
-    <ul>
-      {images.map((item, i) => (
-        <li key={i}><img src={process.env.PUBLIC_URL + item.url} alt={item.alt} /></li>
-      ))}
-    </ul>
+    <div className="content-unit">
+      <GalleryGroup items={images} />
+    </div>
   );
 }
 
