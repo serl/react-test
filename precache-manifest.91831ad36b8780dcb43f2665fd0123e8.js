966e271ebdec8bc38bbfe58422fcc4094ebb876d
Sergio Livi <me@serl.it>
2019-06-03 18:15:38 +0200

    Updates

---
+++ precache-manifest.91831ad36b8780dcb43f2665fd0123e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3b04d2205df133826e71e62dde38ce",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "b0c63668e6f97c9f0ce6",
+    "url": "/react-test/static/css/main.0f1d5b2e.chunk.css"
+  },
+  {
+    "revision": "cb3172cd3b50fb1f7f2b",
+    "url": "/react-test/static/js/2.2b91e2f1.chunk.js"
+  },
+  {
+    "revision": "6e929e57768a52a19777",
+    "url": "/react-test/static/js/3.4d51f76b.chunk.js"
+  },
+  {
+    "revision": "2ae058bcbd54d69534a7",
+    "url": "/react-test/static/js/4.5269fdcb.chunk.js"
+  },
+  {
+    "revision": "4a632a5de50d0ae5cb2f",
+    "url": "/react-test/static/js/5.e8e65e78.chunk.js"
+  },
+  {
+    "revision": "01930d28e2959d4db6d1",
+    "url": "/react-test/static/js/6.96308151.chunk.js"
+  },
+  {
+    "revision": "b0c63668e6f97c9f0ce6",
+    "url": "/react-test/static/js/main.d33cf3eb.chunk.js"
+  },
+  {
+    "revision": "8718e2170ea7dec90530",
+    "url": "/react-test/static/js/runtime~main.412cce9a.js"
+  },
+  {
+    "revision": "c555c87843f39c34dcdcb17c67ab20cf",
+    "url": "/react-test/static/media/cover.c555c878.png"
+  },
+  {
+    "revision": "f56a1f15fd274117bbe2929c3cc73099",
+    "url": "/react-test/static/media/drop.f56a1f15.svg"
+  },
+  {
+    "revision": "8ee197aa9b0809e9a4b4eee70b291a02",
+    "url": "/react-test/static/media/flame.8ee197aa.svg"
+  },
+  {
+    "revision": "b279b358d959631e5c9958ded8394b11",
+    "url": "/react-test/static/media/pork.b279b358.svg"
+  },
+  {
+    "revision": "140615dd7122c72575b2160fdb8f0b70",
+    "url": "/react-test/static/media/roll.140615dd.svg"
+  }
+]);
---
+++ static/js/5.e8e65e78.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{22:function(p,o,n){p.exports=n.p+"static/media/pork.b279b358.svg"}}]);
+//# sourceMappingURL=5.e8e65e78.chunk.js.map
