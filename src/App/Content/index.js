9dfcddb59b50fa6afcbd6286ae3ab43424dcce47
Sergio Livi <me@serl.it>
2019-06-04 07:39:54 +0200

    avoid !important to stick yellow boxes, plus the variable now is defined only once

--- src/App/Content/index.js
+++ src/App/Content/index.js
@@ -4,11 +4,18 @@ import Article from './Article';
 import Gallery from '../Gallery';
 import Box from './Box';
 
+const boxesStickTop = 50;
+
 function Content({ articles, gallery, boxes, scrollTop }) {
   const [selectedBox, setSelectedBox] = useState(undefined);
 
   const contentTop = 512 - (scrollTop * .2);
-  const boxesTop = 442 + (scrollTop * .1);
+  let boxesTop = 442 + (scrollTop * .1);
+  let boxesClass = '';
+  if (boxesTop < scrollTop + boxesStickTop) {
+    boxesTop = boxesStickTop;
+    boxesClass = 'stuck';
+  }
 
   return (
     <>
@@ -25,7 +32,7 @@ function Content({ articles, gallery, boxes, scrollTop }) {
           <Gallery items={gallery} />
         </div>
       </div>
-      <div className={`colon boxes-container ${boxesTop < scrollTop + 50 ? 'stuck' : ''}`} style={{ top: boxesTop }}>
+      <div className={`colon boxes-container ${boxesClass}`} style={{ top: boxesTop }}>
         {boxes.map((item, i) => (
           <Box key={i} {...item} fullScreen={selectedBox === i} setFullscreen={state => setSelectedBox(state ? i : undefined)} />
         ))}
